dd25c60137bf733e691d04c84fc14492547f7933
Érik Eiji <eriknibe1@gmail.com>
2023-08-14 17:26:20 -0300

    feat: add AssignIconsToSquares function

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,32 @@ namespace MatchingGame
         public Form1()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor; 
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
     }
 }
